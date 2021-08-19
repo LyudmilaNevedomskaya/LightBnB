88225cd57c953c04a015481436e6dd2ed4a0f29e
Lyudmila Nevedomskaya <milo4kamoya@gmail.com>
2021-08-19 00:18:00 +0000

    moved all inserts into one file

--- seeds/04_seeds.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
-VALUES (1, 2, 9, 9, 'good'),
-       (3, 7, 11, 3, 'not good'),
-       (5, 1, 12, 5, 'good'),
-       (7, 3, 11, 8, 'good'),
-       (4, 4, 8, 7, 'exellent');
